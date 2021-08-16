86fac16767f601ad9f9d9338d836fc3ebb14fb03
Sabrina Sulong <sabrina.sulong@thoughtworks.com>
2021-08-16 16:07:40 +0800

    Fix implementation of setTodo and deleteTodo
    Both functions to take in id of each TodoItem

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -18,20 +18,22 @@ const TodoList = ({ id, title, deleteTodoList }) => {
   ]);
 
   const [newItemName, setNewItemName] = useState("");
+
   const handleChange = (e) => {
     setNewItemName(e.target.value);
   };
 
   function addNewTodo() {
-    const newTodoItems = {
+    const newTodoItem = {
       id: uuidv4(),
       name: newItemName,
       isDone: false,
     };
-    setTodoItems([...todoItems, newTodoItems]);
+
+    setTodoItems([...todoItems, newTodoItem]);
   }
 
-  function createSetTodo(id) {
+  function setTodo(id) {
     const todoIndex = todoItems.findIndex((todo) => todo.id === id);
     const updatedTodos = [...todoItems];
     updatedTodos[todoIndex].isDone = !updatedTodos[todoIndex].isDone;
@@ -42,29 +44,6 @@ const TodoList = ({ id, title, deleteTodoList }) => {
   function deleteTodo(id) {
     const updatedToDoItems = todoItems.filter((todo) => todo.id !== id);
     setTodoItems(updatedToDoItems);
-  }
-
-  function displayTodos() {
-    // console.log(this.props);
-
-    return todoItems.map((todo) => {
-      // 4. add a method that can change the status of isDone
-      const setTodo = createSetTodo(todo.id);
-
-      // add a delete function
-      const setdeleteTodo = deleteTodo(todo.id);
-
-      // 5. pass in the method as a prop to Todo component
-      return (
-        <TodoItem
-          key={todo.id}
-          name={todo.name}
-          isDone={todo.isDone}
-          setTodo={setTodo}
-          deleteTodo={setdeleteTodo} // add it as a prop
-        />
-      );
-    });
   }
 
   return (
@@ -80,7 +59,16 @@ const TodoList = ({ id, title, deleteTodoList }) => {
         placeholder="Take a break"
       />
       <button onClick={() => addNewTodo()}>add</button>
-      <div className="items">{displayTodos()}</div>
+      <div className="items">
+        {todoItems.map((todo) => {
+          return (<TodoItem 
+          key={todo.id}
+          name={todo.name}
+          isDone={todo.isDone}
+          setTodo={() => setTodo(todo.id)}
+          deleteTodo={() => deleteTodo(todo.id)}/>)})
+        }
+      </div>
     </div>
   );
 };
